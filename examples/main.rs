41375e8a3cba4aabf7008df831dedc98078dcb85
sakikomikado <komietty@gmail.com>
2025-11-03 18:11:03 +0900

    some refactorings

--- examples/main.rs
+++ examples/main.rs
@@ -1,16 +1,20 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+use std::time::Instant;
 use bevy::asset::RenderAssetUsages;
 use bevy::prelude::*;
 use bevy::pbr::wireframe::{WireframePlugin, Wireframe, WireframeColor};
 use bevy::render::mesh::Indices;
 use bevy::color::palettes::css::*;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
-use mfd::{compute_boolean, Manifold};
-use mfd::common::OpType;
+use boolean::{compute_boolean, Manifold};
+use boolean::boolean03::boolean03;
+use boolean::boolean45::boolean45;
+use boolean::common::OpType;
 
-#[derive(Resource)]
-struct MfdHandle0(Manifold);
-#[derive(Resource)]
-struct MfdHandle1(Manifold);
+#[derive(Resource)] struct MfdHandle0(Manifold);
+#[derive(Resource)] struct MfdHandle1(Manifold);
 
 #[derive(Component)]
 struct ToggleableMesh;
@@ -43,9 +47,10 @@ fn toggle_mesh_visibility(
         };
     };
     let mut vis: Vec<_> = query.iter_mut().collect();
-    if keyboard.just_pressed(KeyCode::Space)  { cb(&mut vis[1]); cb(&mut vis[2]); }
-    if keyboard.just_pressed(KeyCode::Digit0) { cb(&mut vis[1]); }
-    if keyboard.just_pressed(KeyCode::Digit1) { cb(&mut vis[2]); }
+    if keyboard.just_pressed(KeyCode::Space)  { cb(&mut vis[0]); cb(&mut vis[1]); cb(&mut vis[2]); }
+    if keyboard.just_pressed(KeyCode::Digit0) { cb(&mut vis[0]); }
+    if keyboard.just_pressed(KeyCode::Digit1) { cb(&mut vis[1]); }
+    if keyboard.just_pressed(KeyCode::Digit2) { cb(&mut vis[2]); }
 }
 
 fn main() {
@@ -71,9 +76,11 @@ fn setup(
 
     let (m0, _) = tobj::load_obj("assets/models/cube_x_plus.obj", &tobj::LoadOptions { ..Default::default() }).expect("failed");
     let (m1, _) = tobj::load_obj("assets/models/fertility_004.obj", &tobj::LoadOptions { ..Default::default() }).expect("failed");
-    let (m2, _) = tobj::load_obj("assets/models/gargoyle.obj", &tobj::LoadOptions { ..Default::default() }).expect("failed");
+    let (m2, _) = tobj::load_obj("assets/models/double-torus.obj", &tobj::LoadOptions { ..Default::default() }).expect("failed");
+
+    let bgn = Instant::now();
     let mut mfs = vec![];
-    for (m, s) in vec![(m0, 1.), (m1, 1.), (m2, 2.)] {
+    for (m, s) in vec![(m0, 1.), (m1, 0.7), (m2, 2.)] {
         let mesh = &m[0].mesh;
         let pos_buf = mesh.positions.iter().map(|&v| (v * s) as f64).collect::<Vec<f64>>();
         let idx_buf = mesh.indices.iter().map(|&v| v as usize).collect::<Vec<usize>>();
@@ -81,43 +88,35 @@ fn setup(
         mfs.push(mf);
     }
 
+    let res = compute_boolean(&mfs[0], &mfs[1], OpType::Subtract).unwrap();
+    let res = compute_boolean(&res, &mfs[2], OpType::Subtract).unwrap();
+    let dur = bgn.elapsed();
+    println!("{} ms", dur.as_millis());
+    //let res = compute_boolean(&res, &mfs[2], OpType::Subtract).unwrap();
+    //let _ = save_obj(
+    //    "assets/models/boolean_03.obj",
+    //    &res.ps.iter().map(|p| [p.x as f32, p.y as f32, p.z as f32]).collect::<Vec<_>>(),
+    //    &res.hs.chunks(3).map(|hs| [hs[0].tail, hs[1].tail, hs[2].tail]).collect::<Vec<_>>(),
+    //    None,
+    //    None,
+    //);
+    //let res2 = compute_boolean(&res1, &mfs[3], OpType::Subtract).unwrap();
 
-    let res0 = compute_boolean(&mfs[0], &mfs[1], OpType::Subtract).unwrap();
-    let res1 = compute_boolean(&res0, &mfs[2], OpType::Subtract).unwrap();
+    //let b03 = boolean03(&mfs[0], &mfs[1], &OpType::Subtract);
+    //let b45 = boolean45(&mfs[0], &mfs[1], &b03, &OpType::Subtract);
+    //let bgn = b45.initial_hid_per_faces[9] as usize;
+    //let end = b45.initial_hid_per_faces[10] as usize;
     //let mut edges = vec![];
-    //for h in temp.iter() {
-    //    let p0 = pos[h.x];
-    //    let p1 = pos[h.y];
-    //    edges.push((
-    //        Vec3::new(p0[0] as f32, p0[1] as f32, p0[2] as f32),
-    //        Vec3::new(p1[0] as f32, p1[1] as f32, p1[2] as f32),
-    //    ));
+    //for (i, h) in b45.hs.iter().enumerate() {
+    //    if i >= bgn && i < end {
+    //        let p0 = b45.ps[h.tail];
+    //        let p1 = b45.ps[h.head];
+    //        edges.push((
+    //            Vec3::new(p0[0] as f32, p0[1] as f32, p0[2] as f32),
+    //            Vec3::new(p1[0] as f32, p1[1] as f32, p1[2] as f32),
+    //        ));
+    //    }
     //}
-
-    {
-        let mut bm = Mesh::new(
-            bevy::render::mesh::PrimitiveTopology::TriangleList,
-            RenderAssetUsages::default()
-        );
-
-        bm.insert_indices(Indices::U32(
-            res1.hs.chunks(3).flat_map(|t| [t[0].tail as u32, t[1].tail as u32, t[2].tail as u32]).collect()));
-        bm.insert_attribute(
-            Mesh::ATTRIBUTE_POSITION,
-            res1.ps.iter().map(|p| [p[0] as f32, p[1] as f32, p[2] as f32]).collect::<Vec<_>>()
-        );
-
-        cmds.spawn((
-            Mesh3d(meshes.add(bm).clone()),
-            MeshMaterial3d(mats.add(StandardMaterial { ..default()})),
-            Transform::default(),
-            Wireframe,
-            WireframeColor { color: GRAY.into() },
-            ToggleableMesh,
-        ));
-    }
-    /*
-    */
 
     cmds.insert_resource(
         DrawingData{ units: vec![
@@ -139,23 +138,43 @@ fn setup(
             mf.ps.iter().map(|p| [p.x as f32, p.y as f32, p.z as f32]).collect::<Vec<_>>()
         );
 
-        //cmds.spawn((
-        //    Mesh3d(meshes.add(bm).clone()),
-        //    MeshMaterial3d(mats.add(StandardMaterial { ..default() })),
-        //    Transform::default(),
-        //    Wireframe,
-        //    WireframeColor { color: GRAY.into() },
-        //    ToggleableMesh,
-        //));
+        cmds.spawn((
+            Mesh3d(meshes.add(bm).clone()),
+            MeshMaterial3d(mats.add(StandardMaterial { ..default() })),
+            Transform::default(),
+            Wireframe,
+            WireframeColor { color: GRAY.into() },
+            ToggleableMesh,
+        ));
     }
 
 
+    let mf2 = mfs.pop().expect("missing third manifold");
     let mf1 = mfs.pop().expect("missing second manifold");
     let mf0 = mfs.pop().expect("missing first manifold");
     cmds.insert_resource(MfdHandle0(mf0));
     cmds.insert_resource(MfdHandle1(mf1));
+    cmds.insert_resource(MfdHandle1(mf2));
+
     cmds.spawn((PointLight::default(), Transform::from_xyz(3., 4., 3.)));
     cmds.spawn((Transform::from_translation(Vec3::new(0., 1.5, 5.)), PanOrbitCamera::default(),));
+
+    let mut bm = Mesh::new(
+        bevy::render::mesh::PrimitiveTopology::TriangleList,
+        RenderAssetUsages::default()
+    );
+    bm.insert_indices(Indices::U32(res.hs.chunks(3).flat_map(|t| [t[0].tail as u32, t[1].tail as u32, t[2].tail as u32]).collect()));
+    bm.insert_attribute(Mesh::ATTRIBUTE_POSITION, res.ps.iter().map(|p| [p[0] as f32, p[1] as f32, p[2] as f32]).collect::<Vec<_>>());
+    cmds.spawn((
+        Mesh3d(meshes.add(bm).clone()),
+        MeshMaterial3d(mats.add(StandardMaterial { ..default()})),
+        Transform::default(),
+        Wireframe,
+        WireframeColor { color: GRAY.into() },
+        ToggleableMesh,
+    ));
+    /*
+    */
 }
 
 fn draw_example_collection(
@@ -200,4 +219,72 @@ fn draw_example_collection(
             );
         }
     */
+}
+
+
+pub fn save_obj(
+    path: impl AsRef<Path>,
+    pos: &[[f32; 3]],
+    idx: &[[usize; 3]],
+    nml: Option<&[[f32; 3]]>,
+    uvs: Option<&[[f32; 2]]>,
+) -> std::io::Result<()> {
+    let file = File::create(path)?;
+    let mut w = BufWriter::new(file);
+
+    for p in pos {
+        writeln!(&mut w, "v {:.8} {:.8} {:.8}", p[0], p[1], p[2])?;
+    }
+
+    if let Some(ts) = uvs {
+        for t in ts { writeln!(&mut w, "vt {:.8} {:.8}", t[0], t[1])?; }
+    }
+
+    if let Some(ns) = nml {
+        for n in ns { writeln!(&mut w, "vn {:.8} {:.8} {:.8}", n[0], n[1], n[2])?; }
+    }
+
+    match (uvs.is_some(), nml.is_some()) {
+        (true, true) => {
+            for tri in idx {
+                writeln!(
+                    &mut w,
+                    "f {}/{}/{} {}/{}/{} {}/{}/{}",
+                    tri[0] + 1, tri[0] + 1, tri[0] + 1,
+                    tri[1] + 1, tri[1] + 1, tri[1] + 1,
+                    tri[2] + 1, tri[2] + 1, tri[2] + 1,
+                )?;
+            }
+        }
+        (true, false) => {
+            for tri in idx {
+                writeln!(
+                    &mut w,
+                    "f {}/{} {}/{} {}/{}",
+                    tri[0] + 1, tri[0] + 1,
+                    tri[1] + 1, tri[1] + 1,
+                    tri[2] + 1, tri[2] + 1,
+                )?;
+            }
+        }
+        (false, true) => {
+            for tri in idx {
+                writeln!(
+                    &mut w,
+                    "f {}//{} {}//{} {}//{}",
+                    tri[0] + 1, tri[0] + 1,
+                    tri[1] + 1, tri[1] + 1,
+                    tri[2] + 1, tri[2] + 1,
+                )?;
+            }
+        }
+        (false, false) => {
+            for tri in idx {
+                writeln!(&mut w, "f {} {} {}", tri[0] + 1, tri[1] + 1, tri[2] + 1)?;
+            }
+        }
+    }
+
+    w.flush()?;
+    Ok(())
 }
